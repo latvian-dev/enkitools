38d1015f8c822cdbcf7dd1b6d2bc8aab51539bca
LatvianModder <latvianmodder@gmail.com>
2015-06-02 10:57:27 +0300

    Fixed /spawn in other dimensions bug

--- src/main/java/latmod/enkitools/cmd/CmdSpawn.java
+++ src/main/java/latmod/enkitools/cmd/CmdSpawn.java
@@ -4,6 +4,8 @@ import latmod.core.Teleporter;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.DimensionManager;
 
 public class CmdSpawn extends CmdEnki
 {
@@ -13,12 +15,13 @@ public class CmdSpawn extends CmdEnki
 	public String onCommand(ICommandSender ics, String[] args)
 	{
 		EntityPlayerMP ep = getCommandSenderAsPlayer(ics);
-		ChunkCoordinates spawn = ep.worldObj.getSpawnPoint();
+		WorldServer w = DimensionManager.getWorld(0);
+		ChunkCoordinates spawn = w.getSpawnPoint();
 		
-		while(ep.worldObj.getBlock(spawn.posX, spawn.posY, spawn.posZ).getCollisionBoundingBoxFromPool(ep.worldObj, spawn.posX, spawn.posY, spawn.posZ) != null)
-			spawn.posY++;
+		//while(ep.worldObj.getBlock(spawn.posX, spawn.posY, spawn.posZ).getCollisionBoundingBoxFromPool(w, spawn.posX, spawn.posY, spawn.posZ) != null)
+		//	spawn.posY++;
 		
-		if(Teleporter.travelEntity(ep, spawn.posX + 0.5D, spawn.posY + 1D, spawn.posZ + 0.5D, ep.worldObj.provider.dimensionId))
+		if(Teleporter.travelEntity(ep, spawn.posX + 0.5D, spawn.posY + 1D, spawn.posZ + 0.5D, 0))
 			return FINE + "Teleported to spawn";
 		return "Failed to teleport!";
 	}
